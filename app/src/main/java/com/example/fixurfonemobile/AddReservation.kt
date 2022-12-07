94efe66d3cf44ff4a53cc85e9ba65304aab9cd85
OonKhengHuang <oonkh-pm19@student.tarc.edu.my>
2022-12-07 19:06:12 +0800

    Fix register device bug 7/12

--- app/src/main/java/com/example/fixurfonemobile/AddReservation.kt
+++ app/src/main/java/com/example/fixurfonemobile/AddReservation.kt
@@ -31,8 +31,9 @@ class AddReservation : Fragment() {
     private var valid = true
     private var timeSelected = ""
     private var deviceSelected = ""
-    private lateinit var custID:String
+    private lateinit var custID: String
     private lateinit var binding: FragmentAddReservationBinding
+    private lateinit var arrayAdapter: ArrayAdapter<String>
     var cal = Calendar.getInstance()
     private var thisContext = this.context
 
@@ -41,15 +42,16 @@ class AddReservation : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_add_reservation, container, false )
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_add_reservation, container, false)
         val sharedPrefFile = "loginSharedPreferences"
         val sharedPreferences: SharedPreferences =
             requireActivity().getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
         custID = sharedPreferences.getString("userUID", null).toString()
 
         //logout id unauthorized user login
-        if(custID == null){
-            val intent = Intent(activity,Login::class.java)
+        if (custID == null) {
+            val intent = Intent(activity, Login::class.java)
             startActivity(intent)
             ActivityCompat.finishAffinity(this.requireActivity())
         }
@@ -57,15 +59,12 @@ class AddReservation : Fragment() {
         //*************************************************** On Create ***************************************************************
         var deviceList: MutableList<String> = mutableListOf()
         var dbRef: DatabaseReference = FirebaseDatabase.getInstance().getReference("Device")
-        dbRef.addValueEventListener(object: ValueEventListener {
+        dbRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                if(snapshot.exists())
-                {
-                    for(v in snapshot.children)
-                    {
+                if (snapshot.exists()) {
+                    for (v in snapshot.children) {
                         var deviceData = v.getValue(Device::class.java)
-                        if(deviceData?.custID== custID)
-                        {
+                        if (deviceData?.custID == custID) {
                             deviceList.add(deviceData.deviceName!!)
                             binding.spinnerPhoneModel.visibility = View.VISIBLE
                             binding.phoneModel.visibility = View.GONE
@@ -75,28 +74,42 @@ class AddReservation : Fragment() {
                     deviceList.add("Others")
 
                     val spinner = binding.spinnerPhoneModel
-                    val arrayAdapter = ArrayAdapter<String>(activity?.application!!,android.R.layout.simple_spinner_dropdown_item,deviceList)
-                    spinner.adapter = arrayAdapter
-                    spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-                        override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                            //Toast.makeText(applicationContext,"selected bank is = "+bank[p2], Toast.LENGTH_SHORT).show()
-                            deviceSelected = deviceList[p2]
-                            if(deviceSelected == "Others"){
-                                binding.phoneModel.visibility = View.VISIBLE
-                            }else{
-                                binding.phoneModel.visibility = View.GONE
+                    try {
+                        arrayAdapter = ArrayAdapter<String>(
+                            activity?.application!!,
+                            android.R.layout.simple_spinner_dropdown_item,
+                            deviceList
+                        )
+                        spinner.adapter = arrayAdapter
+                        spinner.onItemSelectedListener =
+                            object : AdapterView.OnItemSelectedListener {
+                                override fun onItemSelected(
+                                    p0: AdapterView<*>?,
+                                    p1: View?,
+                                    p2: Int,
+                                    p3: Long
+                                ) {
+                                    //Toast.makeText(applicationContext,"selected bank is = "+bank[p2], Toast.LENGTH_SHORT).show()
+                                    deviceSelected = deviceList[p2]
+                                    if (deviceSelected == "Others") {
+                                        binding.phoneModel.visibility = View.VISIBLE
+                                    } else {
+                                        binding.phoneModel.visibility = View.GONE
+                                    }
+                                }
+
+                                override fun onNothingSelected(p0: AdapterView<*>?) {
+
+                                }
+
                             }
-                        }
-
-                        override fun onNothingSelected(p0: AdapterView<*>?) {
-
-                        }
+                    } catch (ex: Exception) {
 
                     }
 
-
                 }
             }
+
             override fun onCancelled(error: DatabaseError) {
                 TODO("Not yet implemented")
             }
@@ -104,12 +117,16 @@ class AddReservation : Fragment() {
 
         })
 
-        val selectTime = arrayOf("10:00 AM","12:00 PM","2:00 PM","4:00 PM")
+        val selectTime = arrayOf("10:00 AM", "12:00 PM", "2:00 PM", "4:00 PM")
 
         val spinner = binding.spinner
-        val arrayAdapter = ArrayAdapter<String>(this.requireContext(),android.R.layout.simple_spinner_dropdown_item,selectTime)
+        val arrayAdapter = ArrayAdapter<String>(
+            this.requireContext(),
+            android.R.layout.simple_spinner_dropdown_item,
+            selectTime
+        )
         spinner.adapter = arrayAdapter
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
                 //Toast.makeText(applicationContext,"selected bank is = "+bank[p2], Toast.LENGTH_SHORT).show()
                 timeSelected = selectTime[p2]
@@ -122,55 +139,64 @@ class AddReservation : Fragment() {
         }
 
         //***********************************************Book Reservation**********************************************************
-        binding.submit.setOnClickListener{
+        binding.submit.setOnClickListener {
             textValidation()
-            if(valid){
+            if (valid) {
                 val fomatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.US)
                 val date = fomatter.format(cal.timeInMillis)
                 var phone = ""
-                if(deviceSelected == "Others"){
-                    phone =binding.phoneModel.text.toString()
-                }else
-                {
+                if (deviceSelected == "Others") {
+                    phone = binding.phoneModel.text.toString()
+                } else {
                     phone = deviceSelected
+                }
+
+                if (deviceSelected == null || deviceSelected == "") {
+                    phone = binding.phoneModel.text.toString()
                 }
 
                 val time = timeSelected
                 var remark = ""
                 var serviceType: MutableList<String> = mutableListOf()
-                if(!TextUtils.isEmpty(binding.remark.text.toString()))
-                {
+                if (!TextUtils.isEmpty(binding.remark.text.toString())) {
                     remark = binding.remark.text.toString()
                 }
-                if(binding.battery.isChecked)
-                {
+                if (binding.battery.isChecked) {
                     serviceType.add("Battery Replacement")
                 }
-                if(binding.screen.isChecked)
-                {
+                if (binding.screen.isChecked) {
                     serviceType.add("Screen Service")
                 }
-                if(binding.camera.isChecked)
-                {
+                if (binding.camera.isChecked) {
                     serviceType.add("Camera Replacement")
                 }
-                if(binding.speaker.isChecked)
-                {
+                if (binding.speaker.isChecked) {
                     serviceType.add("Speaker/Microphone")
                 }
-                if(binding.others.isChecked)
-                {
+                if (binding.others.isChecked) {
                     serviceType.add("Others")
                 }
                 var datenow: GetTodayDate = GetTodayDate()
-                val reserve = Reservation("RE"+ UUID.randomUUID().toString().replace("-",""),custID,null,datenow.getTodayDate(),
-                    date, time,serviceType,phone,if(remark == "") null else remark,"Pending","Pending")
+                val reserve = Reservation(
+                    "RE" + UUID.randomUUID().toString().replace("-", ""),
+                    custID,
+                    null,
+                    datenow.getTodayDate(),
+                    date,
+                    time,
+                    serviceType,
+                    phone,
+                    if (remark == "") null else remark,
+                    "Pending",
+                    "Pending"
+                )
                 var db = ReservationDB()
                 db.addReservation(reserve)
                 val intent = Intent(activity, MainActivity::class.java)
 
                 //************************************* Dialog ********************************************************
-                val view = View.inflate(this.requireActivity(), R.layout.reservation_success_dialog, null)
+                val view =
+                    View.inflate(this.requireActivity(), R.layout.reservation_success_dialog, null)
                 val builder = AlertDialog.Builder(this.requireActivity())
                 builder.setView(view)
 
@@ -178,8 +204,12 @@ class AddReservation : Fragment() {
                 dialog.show()
                 dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
                 var btnCancel: Button = view.findViewById(R.id.btn_cancel)
-                Toast.makeText(this.requireActivity(),"Reservation Added successful", Toast.LENGTH_SHORT).show()
-                btnCancel.setOnClickListener{
+                Toast.makeText(
+                    this.requireActivity(),
+                    "Reservation Added successful",
+                    Toast.LENGTH_SHORT
+                ).show()
+                btnCancel.setOnClickListener {
                     dialog.dismiss()
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
                     startActivity(intent)
@@ -190,58 +220,55 @@ class AddReservation : Fragment() {
 
         }
         val dateSetListener = object : DatePickerDialog.OnDateSetListener {
-            override fun onDateSet(view:DatePicker?,year: Int, month: Int, dayOfMonth: Int)
-            {
-                cal.set(year,month,dayOfMonth)
+            override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+                cal.set(year, month, dayOfMonth)
                 val fomatter = SimpleDateFormat("dd/MM/yyyy", Locale.US)
                 binding.bookingDate.setText(fomatter.format(cal.timeInMillis))
 
             }
         }
 
-        binding.bookingDate.setOnClickListener{
-            DatePickerDialog(this.requireContext(),R.style.DialogTheme,
+        binding.bookingDate.setOnClickListener {
+            DatePickerDialog(
+                this.requireContext(), R.style.DialogTheme,
                 dateSetListener,
                 // set DatePickerDialog to point to today's date when it loads up
                 cal.get(Calendar.YEAR),
                 cal.get(Calendar.MONTH),
-                cal.get(Calendar.DAY_OF_MONTH)).show()
+                cal.get(Calendar.DAY_OF_MONTH)
+            ).show()
 
         }
         return binding.root
     }
 
-    private fun textValidation(){
+    private fun textValidation() {
         val fomatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.US)
         val date = fomatter.format(cal.timeInMillis)
         val datenow = GetTodayDate()
-        if( TextUtils.isEmpty(binding.bookingDate.text.toString())){
-            binding.bookingDate.setError( "Booking Date is required!" )
+        if (TextUtils.isEmpty(binding.bookingDate.text.toString())) {
+            binding.bookingDate.setError("Booking Date is required!")
             valid = valid && false
-        }else{
+        } else {
             valid = true
         }
 
-        if( TextUtils.isEmpty(binding.phoneModel.text.toString())){
-            if(deviceSelected == "Others")
-            {
-                binding.phoneModel.setError( "Phone Model is required!" )
+        if (TextUtils.isEmpty(binding.phoneModel.text.toString())) {
+            if (deviceSelected == "Others") {
+                binding.phoneModel.setError("Phone Model is required!")
                 valid = valid && false
             }
         }
 
-        if(binding.screen.isChecked || binding.battery.isChecked || binding.camera.isChecked || binding.speaker.isChecked || binding.others.isChecked)
-        {
+        if (binding.screen.isChecked || binding.battery.isChecked || binding.camera.isChecked || binding.speaker.isChecked || binding.others.isChecked) {
             valid = valid && true
 
-        }else
-        {
-            binding.selection.setError( "Please select a problem(s)!" )
+        } else {
+            binding.selection.setError("Please select a problem(s)!")
             valid = valid && false
         }
-        if(date < datenow.getTodayDate() )
-        {
-            binding.bookingDate.setError( "Booking Date must be after today!" )
+        if (date < datenow.getTodayDate()) {
+            binding.bookingDate.setError("Booking Date must be after today!")
             valid = valid && false
         }
 
